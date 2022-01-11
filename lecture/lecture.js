fa1a699be8e777233062c6208ed75b6d648b4e3c
JNYLee <junyjuny1021@gmail.com>
2022-01-11 10:57:43 +0900

    숫자야구게임

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -1,38 +1,65 @@
-var numberOne = Math.ceil(Math.random() * 9);
-var numberTwo = Math.ceil(Math.random() * 9);
-var result = numberOne * numberTwo;
-var string = "hello";
-var boolean = true;
-var wordNumber = document.createElement("div");
-wordNumber.textContent = ""
-  .concat(numberOne, " \uACF1\uD558\uAE30 ")
-  .concat(numberTwo, "\uB294?");
-document.body.append(wordNumber);
-var form = document.createElement("form");
-document.body.append(form);
-var input = document.createElement("input");
-input.type = "number";
+var body = document.body;
+var candidate;
+var array = [];
+function chooseNumber() {
+    candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    array = [];
+    for (var i = 0; i < 4; i += 1) {
+        var chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+}
+chooseNumber();
+console.log(array);
+var result = document.createElement('h1');
+body.append(result);
+var form = document.createElement('form');
+body.append(form);
+var input = document.createElement('input');
 form.append(input);
-var button = document.createElement("button");
-button.textContent = "입력!";
+input.type = 'text';
+input.maxLength = 4;
+var button = document.createElement('button');
+button.textContent = '입력!';
 form.append(button);
-var resultDiv = document.createElement("div");
-document.body.append(resultDiv);
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (result === Number(input.value)) {
-    resultDiv.textContent = "딩동댕";
-    numberOne = Math.ceil(Math.random() * 9);
-    numberTwo = Math.ceil(Math.random() * 9);
-    result = numberOne * numberTwo;
-    wordNumber.textContent = ""
-      .concat(numberOne, " \uACF1\uD558\uAE30 ")
-      .concat(numberTwo, "\uB294?");
-    input.value = "";
-    input.focus();
-  } else {
-    resultDiv.textContent = "땡";
-    input.value = "";
-    input.focus();
-  }
+var wrongCount = 0;
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var answer = input.value;
+    if (answer === array.join('')) {
+        result.textContent = '홈런';
+        input.value = '';
+        input.focus();
+        chooseNumber();
+        wrongCount = 0;
+    }
+    else {
+        var answerArray = answer.split('');
+        var strike = 0;
+        var ball = 0;
+        wrongCount += 1;
+        if (wrongCount > 10) {
+            result.textContent = "Over 10times! The Answer is ".concat(array.join(','), ".");
+            input.value = '';
+            input.focus();
+            chooseNumber();
+            wrongCount = 0;
+        }
+        else {
+            console.log('wrong: ', answerArray);
+            for (var i = 0; i <= 3; i += 1) {
+                if (Number(answerArray[i]) === array[i]) {
+                    console.log('Correct digit');
+                    strike += 1;
+                }
+                else if (array.indexOf(Number(answerArray[i])) > -1) {
+                    console.log('Correct number');
+                    ball += 1;
+                }
+            }
+            result.textContent = strike + 'S' + ball + 'B';
+            input.value = '';
+            input.focus();
+        }
+    }
 });
--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -1,41 +1,66 @@
-let numberOne = Math.ceil(Math.random() * 9);
-let numberTwo = Math.ceil(Math.random() * 9);
-let result = numberOne * numberTwo;
-let string = 'hello';
-let boolean = true;
+const { body } = document;
+let candidate: number[];
+let array: number[] = [];
 
-const wordNumber = document.createElement('div');
-wordNumber.textContent = `${numberOne} 곱하기 ${numberTwo}는?`;
-document.body.append(wordNumber);
+function chooseNumber() {
+	candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	array = [];
+	for (let i = 0; i < 4; i += 1) {
+		const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)),1)[0];
+		array.push(chosen);
+	}
+}
 
+chooseNumber();
+console.log(array);
+
+const result = document.createElement('h1');
+body.append(result);
 const form = document.createElement('form');
-document.body.append(form);
-
+body.append(form);
 const input = document.createElement('input');
-input.type = 'number';
 form.append(input);
-
+input.type = 'text';
+input.maxLength = 4;
 const button = document.createElement('button');
 button.textContent = '입력!';
 form.append(button);
 
-const resultDiv = document.createElement('div');
-document.body.append(resultDiv);
-
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	if (result === Number(input.value)) {
-		resultDiv.textContent = '딩동댕';
-		numberOne = Math.ceil(Math.random() * 9);
-		numberTwo = Math.ceil(Math.random() * 9);
-		result = numberOne * numberTwo;
-		wordNumber.textContent = `${numberOne} 곱하기 ${numberTwo}는?`;
+let wrongCount = 0;
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const answer = input.value;
+	if (answer === array.join('')) {
+		result.textContent = '홈런';
 		input.value = '';
 		input.focus();
+		chooseNumber();
+		wrongCount = 0;
 	} else {
-		resultDiv.textContent = '땡';
-		input.value = '';
-		input.focus();
-
+		const answerArray = answer.split('');
+		let strike = 0;
+		let ball = 0;
+		wrongCount += 1;
+		if (wrongCount > 10) {
+			result.textContent = `Over 10times! The Answer is ${array.join(',')}.`;
+			input.value = '';
+			input.focus();
+			chooseNumber();
+			wrongCount = 0;
+		} else {
+			console.log('wrong: ', answerArray);
+			for (let i = 0; i <= 3; i += 1) {
+				if (Number(answerArray[i]) === array[i]) {
+					console.log('Correct digit');
+					strike += 1;
+				} else if (array.indexOf(Number(answerArray[i])) > -1) {
+					console.log('Correct number');
+					ball += 1;
+				}
+			}
+			result.textContent = strike + 'S' + ball + 'B';
+			input.value = '';
+			input.focus();
+		}
 	}
-})
+}); 
