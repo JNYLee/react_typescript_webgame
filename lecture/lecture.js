35e1b5e6be73f79cc087f102e76b335a12c67765
JNYLee <junyjuny1021@gmail.com>
2022-01-11 16:35:58 +0900

    가위바위보게임 사진돌아가도록 설정 및 점수 표시

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -5,46 +5,83 @@ const rsp = {
     SCISSORS: '-142px',
     PAPER: '-284px'
 }; // as const도 설명
-const score = {
-    SCISSORS: 1,
-    ROCK: 0,
-    PAPER: -1,
-};
 function computerChoice(imgCoords) {
     return Object.keys(rsp).find((k) => {
         return rsp[k] === imgCoords;
     });
 }
+let interval;
+function intervalMaker() {
+    interval = setInterval(function () {
+        if (imgCoords === rsp.ROCK) {
+            imgCoords = rsp.SCISSORS;
+        }
+        else if (imgCoords === rsp.SCISSORS) {
+            imgCoords = rsp.PAPER;
+        }
+        else {
+            imgCoords = rsp.ROCK;
+        }
+        if (document.querySelector('#computer')) {
+            document.querySelector('#computer').style.background = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imgCoords + ' 0';
+        }
+    }, 100);
+}
+// intervalMaker();
+const score = {
+    SCISSORS: 1,
+    ROCK: 0,
+    PAPER: -1,
+};
+let clickable = false;
 document.querySelectorAll('.btn').forEach((btn) => {
-    btn.addEventListener('click', function (e) {
-        const myChoice = this.textContent;
+    btn.addEventListener('click', function () {
+        clickable = false;
+        clearInterval(interval); // setInterval 중지
+        setTimeout(() => {
+            clickable = true;
+            intervalMaker();
+        }, 1000);
+        if (!clickable)
+            return;
+        const myChoice = this.id;
         const myScore = score[myChoice];
         const computerScore = score[computerChoice(imgCoords)];
         const diff = myScore - computerScore;
         if (diff === 0) {
-            console.log('Draw');
+            console.log('비겼습니다');
         }
-        else if ([-1, 2].indexOf(diff)) {
-            console.log('Win!');
+        else if ([-1, 2].includes(diff)) {
+            console.log('이겼습니다!!');
+            MS += 1;
+            document.querySelector('#score-board').textContent = `USER: ${MS} :: COM: ${CS}`;
         }
         else {
-            console.log('Lose');
+            console.log('졌습니다 ㅠㅠ.');
+            CS += 1;
+            document.querySelector('#score-board').textContent = `USER: ${MS} :: COM: ${CS}`;
+        }
+        if (MS === 3 || CS === 3) {
+            console.log('Win ', MS > CS ? 'User!' : 'Computer!');
+            clearInterval(interval);
+            clickable = false;
         }
     });
 });
-// let interval: number;
-// function intervalMaker() {
-//   interval = setInterval(function () {
-//     if (imgCoords === rsp.ROCK) {
-//       imgCoords = rsp.SCISSORS;
-//     } else if (imgCoords === rsp.SCISSORS) {
-//       imgCoords = rsp.PAPER;
-//     } else {
-//       imgCoords = rsp.ROCK;
-//     }
-//     if (document.querySelector('#computer')) {
-//       (document.querySelector('#computer') as HTMLElement).style.background = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imgCoords + ' 0';
-//     }
-//   }, 100);
-// }
-// intervalMaker();
+let CS = 0;
+let MS = 0;
+// SCISSORS: 1, ROCK: 0, PAPER: -1
+// 나\컴퓨터    SCISSORS   ROCK    PAPER
+//        SCISSORS   1 1    1 0   1 -1
+//        ROCK   0 1    0 0   0 -1
+//          PAPER  -1 1   -1 0  -1 -1
+let start = 3;
+const interval2 = setInterval(function () {
+    if (start === 0) {
+        console.log('Start!!!');
+        intervalMaker();
+        return clearInterval(interval2);
+    }
+    console.log(start);
+    start -= 1;
+}, 1000);
