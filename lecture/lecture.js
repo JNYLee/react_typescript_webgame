fce4a5b4789fb7d9f90955671f6a4f61a746897e
JNYLee <junyjuny1021@gmail.com>
2022-01-12 17:39:49 +0900

    [웹게임]; 틱택토

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -1,82 +1,76 @@
 "use strict";
-const horizontal = 4;
-const vertical = 3;
-const colors = ['red', 'red', 'orange', 'orange', 'green', 'green', 'yellow', 'yellow', 'white', 'white', 'pink', 'pink'];
-let colorCandidate = colors.slice();
-let color = [];
-let clickFlag = true;
-let clickCard = [];
-let completedCard = [];
-let startTime = null;
-function shuffle() {
-    for (let i = 0; colorCandidate.length > 0; i += 1) {
-        color = color.concat(colorCandidate.splice(Math.floor(Math.random() * colorCandidate.length), 1));
+const table = document.createElement('table');
+const rows = [];
+const cells = [];
+let turn = 'X';
+const result = document.createElement('div');
+let count = 0;
+function callback(event) {
+    const rowIndex = rows.indexOf(event.currentTarget.parentNode);
+    const cellIndex = cells[rowIndex].indexOf(event.currentTarget);
+    count++;
+    if (cells[rowIndex][cellIndex].textContent !== '') {
+        console.log('빈 칸이 아닙니다.');
+    }
+    else {
+        cells[rowIndex][cellIndex].textContent = turn;
+        let full = false;
+        if (cells[rowIndex][0].textContent === turn &&
+            cells[rowIndex][1].textContent === turn &&
+            cells[rowIndex][2].textContent === turn) {
+            full = true;
+        }
+        if (cells[0][cellIndex].textContent === turn &&
+            cells[1][cellIndex].textContent === turn &&
+            cells[2][cellIndex].textContent === turn) {
+            full = true;
+        }
+        if (cells[0][0].textContent === turn &&
+            cells[1][1].textContent === turn &&
+            cells[2][2].textContent === turn) {
+            full = true;
+        }
+        if (cells[0][2].textContent === turn &&
+            cells[1][1].textContent === turn &&
+            cells[2][0].textContent === turn) {
+            full = true;
+        }
+        if (full) {
+            result.textContent = `${turn}님이 승리!`;
+            turn = 'X';
+            cells.forEach((row) => {
+                row.forEach((cell) => {
+                    cell.textContent = '';
+                });
+            });
+            count = 0;
+        }
+        else if (count === 9) {
+            result.textContent = `무승부!`;
+            turn = 'X';
+            cells.forEach((row) => {
+                row.forEach((cell) => {
+                    cell.textContent = '';
+                });
+            });
+            count = 0;
+        }
+        else {
+            turn = turn === 'O' ? 'X' : 'O';
+        }
     }
 }
-function setCard(horizontal, vertical) {
-    clickFlag = false;
-    for (let i = 0; i < horizontal * vertical; i++) {
-        const card = document.createElement('div');
-        card.className = 'card';
-        const cardInner = document.createElement('div');
-        cardInner.className = 'card-inner';
-        const cardFront = document.createElement('div');
-        cardFront.className = 'card-front';
-        const cardBack = document.createElement('div');
-        cardBack.className = 'card-back';
-        cardBack.style.backgroundColor = color[i];
-        cardInner.appendChild(cardFront);
-        cardInner.appendChild(cardBack);
-        card.appendChild(cardInner);
-        card.addEventListener('click', function () {
-            if (clickFlag && !completedCard.includes(this)) {
-                this.classList.toggle('flipped');
-                clickCard.push(this);
-                if (clickCard.length === 2) {
-                    const firstBackground = clickCard[0].querySelector('.card-back').style.backgroundColor;
-                    const secondBackground = clickCard[1].querySelector('.card-back').style.backgroundColor;
-                    if (firstBackground === secondBackground) {
-                        completedCard.push(clickCard[0]);
-                        completedCard.push(clickCard[1]);
-                        clickCard = [];
-                        if (completedCard.length === horizontal * vertical) {
-                            const endTime = new Date().getTime();
-                            alert(`축하합니다! ${(endTime - startTime.getTime()) / 1000}초 걸렸습니다!`);
-                            document.querySelector('#wrapper').innerHTML = '';
-                            colorCandidate = colors.slice();
-                            color = [];
-                            completedCard = [];
-                            startTime = null;
-                            shuffle();
-                            setCard(horizontal, vertical);
-                        }
-                    }
-                    else {
-                        clickFlag = false;
-                        setTimeout(() => {
-                            clickCard[0].classList.remove('flipped');
-                            clickCard[1].classList.remove('flipped');
-                            clickFlag = true;
-                            clickCard = [];
-                        }, 1000);
-                    }
-                }
-            }
-        });
-        document.querySelector('#wrapper').appendChild(card);
+for (let i = 1; i <= 3; i++) {
+    const row = document.createElement('tr');
+    rows.push(row);
+    cells.push([]);
+    for (let j = 1; j <= 3; j++) {
+        const cell = document.createElement('td');
+        cell.addEventListener('click', callback);
+        cells[i - 1].push(cell);
+        row.appendChild(cell);
     }
-    Array.prototype.forEach.call(document.getElementsByClassName('card'), (card, index) => {
-        setTimeout(() => {
-            card.classList.add('flipped');
-        }, 1000 + 100 * index);
-    });
-    setTimeout(() => {
-        Array.prototype.forEach.call(document.getElementsByClassName('card'), (card, index) => {
-            card.classList.remove('flipped');
-        });
-        clickFlag = true;
-        startTime = new Date();
-    }, 5000);
+    table.appendChild(row);
 }
-shuffle();
-setCard(horizontal, vertical);
+document.body.appendChild(table);
+document.body.appendChild(result);
--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -1,88 +1,84 @@
-const horizontal: number = 4;
-const vertical: number = 3;
-const colors: string[] = ['red', 'red', 'orange', 'orange', 'green', 'green', 'yellow', 'yellow', 'white', 'white', 'pink', 'pink'];
+const table: HTMLTableElement = document.createElement('table');
+const rows: HTMLTableRowElement[] = [];
+const cells: HTMLTableCellElement[][] = [];
+let turn: 'O' | 'X' = 'X';
+const result: HTMLDivElement = document.createElement('div');
+let count: number = 0;
 
-let colorCandidate: string[] = colors.slice();
-let color: string[] = [];
-let clickFlag: boolean = true;
-let clickCard: HTMLDivElement[] = [];
-let completedCard: HTMLDivElement[] = [];
-let startTime: Date | null = null;
+function callback(event: MouseEvent) {
+    const rowIndex: number = rows.indexOf((event.currentTarget as HTMLTableCellElement).parentNode as HTMLTableRowElement);
+    const cellIndex: number = cells[rowIndex].indexOf(event.currentTarget as HTMLTableCellElement);
+    count++;
 
-function shuffle(): void {
-  for (let i: number = 0; colorCandidate.length > 0; i += 1) {
-    color = color.concat(colorCandidate.splice(Math.floor(Math.random() * colorCandidate.length), 1));
-  }
-}
+    if (cells[rowIndex][cellIndex].textContent !== '') {
+        console.log('빈 칸이 아닙니다.');
+    } else {
+        cells[rowIndex][cellIndex].textContent = turn;
 
-function setCard(horizontal: number, vertical: number) {
-  clickFlag = false;
-  for (let i: number = 0; i < horizontal * vertical; i++) {
-    const card: HTMLDivElement = document.createElement('div');
-    card.className = 'card';
-    const cardInner: HTMLDivElement = document.createElement('div');
-    cardInner.className = 'card-inner';
-    const cardFront: HTMLDivElement = document.createElement('div');
-    cardFront.className = 'card-front';
-    const cardBack: HTMLDivElement = document.createElement('div');
-    cardBack.className = 'card-back';
-    cardBack.style.backgroundColor = color[i];
-    cardInner.appendChild(cardFront);
-    cardInner.appendChild(cardBack);
-    card.appendChild(cardInner);
-
-    card.addEventListener('click', function (this: HTMLDivElement) {
-      if (clickFlag && !completedCard.includes(this)) {
-        this.classList.toggle('flipped');
-        clickCard.push(this);
-        if (clickCard.length === 2) {
-          const firstBackground: string = (clickCard[0].querySelector('.card-back') as HTMLDivElement).style.backgroundColor;
-          const secondBackground: string = (clickCard[1].querySelector('.card-back') as HTMLDivElement).style.backgroundColor;
-          if (firstBackground === secondBackground) {
-            completedCard.push(clickCard[0]);
-            completedCard.push(clickCard[1]);
-            clickCard = [];
-            if (completedCard.length === horizontal * vertical) {
-              const endTime: number = new Date().getTime();
-              alert(`축하합니다! ${(endTime - startTime!.getTime()) / 1000}초 걸렸습니다!`);
-              (document.querySelector('#wrapper') as HTMLDivElement).innerHTML = '';
-              colorCandidate = colors.slice();
-              color = [];
-              completedCard = [];
-              startTime = null;
-              shuffle();
-              setCard(horizontal, vertical);
-            }
-          } else {
-            clickFlag = false;
-            setTimeout(() => {
-              clickCard[0].classList.remove('flipped');
-              clickCard[1].classList.remove('flipped');
-              clickFlag = true;
-              clickCard = [];
-            }, 1000);
-          }
+        let full: boolean = false;
+        if (
+            cells[rowIndex][0].textContent === turn &&
+            cells[rowIndex][1].textContent === turn &&
+            cells[rowIndex][2].textContent === turn
+        ) {
+            full = true;
         }
-      }
-    });  
-
-    (document.querySelector('#wrapper') as HTMLDivElement).appendChild(card);
-  }
-
-  Array.prototype.forEach.call<HTMLCollectionOf<Element>, [(card: HTMLDivElement, index: number) => void], void>(document.getElementsByClassName('card'), (card, index) => {
-    setTimeout(() => {
-      card.classList.add('flipped');
-    }, 1000 + 100 * index);
-  });
-
-  setTimeout(() => {
-    Array.prototype.forEach.call<HTMLCollectionOf<Element>, [(card: HTMLDivElement, index: number) => void], void>(document.getElementsByClassName('card'), (card, index) => {
-      card.classList.remove('flipped');
-    });
-    clickFlag = true;
-    startTime = new Date();
-  }, 5000);
+        if (
+            cells[0][cellIndex].textContent === turn &&
+            cells[1][cellIndex].textContent === turn &&
+            cells[2][cellIndex].textContent === turn
+        ) {
+            full = true;
+        }
+        if (
+            cells[0][0].textContent === turn &&
+            cells[1][1].textContent === turn &&
+            cells[2][2].textContent === turn
+        ) {
+            full = true;
+        }
+        if (
+            cells[0][2].textContent === turn &&
+            cells[1][1].textContent === turn &&
+            cells[2][0].textContent === turn
+        ) {
+            full = true;
+        }
+        if (full) {
+            result.textContent = `${turn}님이 승리!`;
+            turn = 'X';
+            cells.forEach((row) => {
+                row.forEach((cell) => {
+                    cell.textContent = '';
+                });
+            })
+            count = 0;
+        } else if (count === 9) {
+            result.textContent = `무승부!`;
+            turn = 'X';
+            cells.forEach((row) => {
+                row.forEach((cell) => {
+                    cell.textContent = '';
+                });
+            })
+            count = 0;
+        } else {
+            turn = turn === 'O' ? 'X' : 'O';
+        }
+    }
 }
 
-shuffle();
-setCard(horizontal, vertical);
+for (let i: number = 1; i <= 3; i++) {
+    const row: HTMLTableRowElement = document.createElement('tr');
+    rows.push(row);
+    cells.push([]);
+    for (let j: number = 1; j <= 3; j++) {
+        const cell: HTMLTableCellElement = document.createElement('td');
+        cell.addEventListener('click', callback);
+        cells[i - 1].push(cell);
+        row.appendChild(cell);
+    }
+    table.appendChild(row);
+}
+document.body.appendChild(table);
+document.body.appendChild(result);
